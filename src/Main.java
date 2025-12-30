57e16c45a2db328d5ec84142bb71e6288bef7bcc
Student_8 <codecat.ua@gmail.com>
2025-12-30 19:32:17 +0200

    added Some obyekts

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         User Andrey = new User(123456789,"Андрей", "пупкін",1488);
+        User Vova= new User(123456787,"Вова", "васьікн",1000);
+        User Kost= new User(123456786,"Костя", "вовкін",999);
+        User Perfarator= new User(123456785,"Перфатор", "костькін",997);
     }
 }
