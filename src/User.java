3085fddff31add60212112fb0771bf0bfe59c4b7
Student_8 <codecat.ua@gmail.com>
2025-12-30 19:21:02 +0200

    added constructor

--- src/User.java
+++ src/User.java
@@ -2,6 +2,12 @@ public class User {
      public int ID;
      public String name;
      public String lastName;
-
+     public int sum;
+ User(int ID,String name,String lastName, int sum){
+     this.ID = ID;
+     this.lastName = lastName;
+     this.name = name;
+     this.sum = sum;
+ }
 
 }
